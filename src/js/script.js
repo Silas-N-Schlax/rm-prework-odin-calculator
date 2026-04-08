1820283d602bc2e8c82710b17575d41fc560e344
SilasNS <silasschlax@gmail.com>
2026-04-08 12:28:29 -0400

    Write basic functions
    Write add, subtract, multiply, divide, and operate functions to control
    the calculator calculations. Add event listeners to each button and
    update display with user input to be used later.

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,39 @@
 
+let buttons = document.querySelectorAll('.btn')
+buttons.forEach((button) => {
+  button.addEventListener('click', (e) => { updateDisplayWithUserInput(e.target.id)})
+})
+
+
+
+
+function updateDisplayWithUserInput(input) {
+  let display = document.querySelector('.display')
+  if (display.textContent === "0") {
+    display.textContent = input
+  } else {
+    display.textContent += ` ${input} `
+  }
+}
+
+
+
+function operate(a, opperator, b) {
+  switch (opperator) {
+    case ("+"): {
+      return add(a,b) 
+    } case ("-"): {
+      return subtract(a,b)
+    } case ("x"): {
+      return multiply(a,b)
+    } case ("÷"): {
+      return divide(a,b)
+    }
+  }
+}
+
+
+function add (a,b) { return a + b }
+function subtract (a,b) { return a - b }
+function multiply (a,b) { return a * b }
+function divide (a,b) { return a / b }
